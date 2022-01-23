0ddfe1840eb754efcf7ecd993dd7219ae21ccdea
BrenyaSkaggs <93018678+BrenyaSkaggs@users.noreply.github.com>
2022-01-23 15:19:07 -0600

    Update apps_1.js

--- JS/apps_1.js
+++ JS/apps_1.js
@@ -5,23 +5,25 @@ const tableData = data;
 var tbody = d3.select("tbody");
 
 
-// First, clear out any existing data
 function buildTable(data) {
-    tbody.html("");
-  }
+  // First, clear out any existing data
+  tbody.html("");
+
   // Next, loop through each object in the data
   // and append a row and cells for each value in the row
   data.forEach((dataRow) => {
-    // Append a row to the table body      
+    // Append a row to the table body
     let row = tbody.append("tr");
+
     // Loop through each field in the dataRow and add
-    // each value as a table cell (td)    
+    // each value as a table cell (td)
     Object.values(dataRow).forEach((val) => {
       let cell = row.append("td");
       cell.text(val);
       }
     );
-  });  
+  });
+}
 
 
 function handleClick () {
